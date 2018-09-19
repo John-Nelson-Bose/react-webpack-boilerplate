a7e9c27d14b1e895fdefd64644098acca4fa3e21
John Nelson <John_Nelson@bose.com>
2018-09-19 09:29:44 -0400

    Add boilerplate code

---
+++ src/js/App.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+
+    render () {
+
+        return (
+            <div className="container">
+                <p>Boilerplate Application</p>
+            </div>
+        );
+
+    }
+
+}
+
+export default App;
