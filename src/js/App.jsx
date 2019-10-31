bb9d197528bbfa45ba4e29bd6c398a1eb6ac47a0
John Nelson <John_Nelson@bose.com>
2019-10-31 11:35:15 -0400

    Update boilerplate

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 
+import BoseLogo from '../resources/images/Bose_Logo.png';
+
+import '../css/App.css';
+
 class App extends Component {
 
     render () {
 
         return (
             <div className="container">
+                <img src={BoseLogo}/>
                 <p>Boilerplate Application</p>
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -2,81 +2,109 @@ var webpack = require("webpack");
 var path = require("path");
 
 var HtmlWebpackPlugin = require("html-webpack-plugin");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var OpenBrowserPlugin = require("open-browser-webpack-plugin");
-var CopyWebpackPlugin = require("copy-webpack-plugin");
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 var ROOT_PATH = path.resolve(__dirname);
 var ENTRY_PATH = path.resolve(ROOT_PATH, "src/js/index");
 var TEMPLATE_PATH = path.resolve(ROOT_PATH, "src/index.html");
 var BUILD_PATH = path.resolve(ROOT_PATH, "build");
 
-const PORT = 8080;
+const IS_PROD = process.env.NODE_ENV === 'production';
+
+const PORT = 3000;
 
 var config = {
   entry: ENTRY_PATH,
-  devtool: "source-map",
+  output: {
+    path: BUILD_PATH,
+    filename: IS_PROD ? 'js/bundle.min.js' : 'js/bundle.js',
+    publicPath: IS_PROD ? './' : '/'
+  },
+  mode: IS_PROD ? 'production' : 'development',
+  devtool: IS_PROD ? false : 'source-map',
+  devServer: {
+    compress: true,
+    hot: true,
+    inline: true,
+    open: true,
+    port: PORT,
+    overlay: true,
+    historyApiFallback: true
+  },
+  optimization: {
+      minimize: true
+  },
+  performance: {
+      hints: false
+  },
+  resolve: {
+    extensions: [
+      ".js",
+      ".jsx"
+    ]
+  },
   plugins: [
-    new CopyWebpackPlugin([
-      {
-        from: "./src/images",
-        to: "./images"
-      }
-    ]),
     new HtmlWebpackPlugin({
-      inject: "body",
-      template: TEMPLATE_PATH
+      inject: true,
+      template: TEMPLATE_PATH,
+      filename: './index.html',
+      favicon: './src/resources/images/favicon.ico'
     }),
-    new ExtractTextPlugin("css/styles.css"),
-    new OpenBrowserPlugin({
-      url: `http://localhost:${PORT}/`
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[id].css'
     }),
     new webpack.HotModuleReplacementPlugin()
   ],
-  output: {
-    path: BUILD_PATH,
-    filename: "bundle.js",
-    publicPath: "/"
-  },
-  devServer: {
-    historyApiFallback: true
-  },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          cacheDirectory: true,
-          presets: [
-            "babel-preset-env",
-            "babel-preset-react",
-            "babel-preset-stage-2"
-          ]
+    rules: [
+        {
+            test: /\.(js|jsx)$/,
+            use: [
+                {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ]
+                    }
+                }
+            ]
+        },
+        {
+            test: /\.html$/,
+            use: [
+                {
+                    loader: 'html-loader'
+                }
+            ]
+        },
+        {
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader'
+            ]
+        },
+        {
+            test: /\.(jpe?g|png|gif|svg|ico)$/i,
+            use: [
+                {
+                    loader: 'file-loader?name=images/[name].[ext]'
+                }
+            ]
+        },
+        {
+            test: /\.(ttf|otf)$/i,
+            loader: 'file-loader',
+            options: {
+              name: 'fonts/[name].[ext]',
+              publicPath: '../'
+            }
         }
-      },
-      {
-        test: /\.jsx?/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.css?$/,
-        use: ExtractTextPlugin.extract({
-          use: "css-loader"
-        })
-      },
-      {
-        test: /\.png$/,
-        exclude: /node_modules/,
-        loader: "file-loader?name=images/[name].[ext]"
-      }
     ]
-  }
+}
 };
 
 module.exports = config;
